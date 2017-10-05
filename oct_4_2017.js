f7c2523dffc7ecf2c15ef76e3704d01ee282c9eb
sbcreates <drawsarahb@gmail.com>
2017-10-05 15:59:15 -0400

    Completed exercise for oct 4 and tested it works.

---
+++ oct_4_2017.js
@@ -0,0 +1,11 @@
+var items = ["purchase domain", "install SSL certificate", "deploy site", "celebrate success"];
+for(var i = 0; i < items.length; i++){
+  console.log( (i+1) + ": " + items[i] );
+}
+
+// output should be
+// Todo:
+// 1: purchase domain
+// 2: install SSL certificate
+// 3: deploy site
+// 4: celebrate success
